ca27dad4ae458e4803910a98bf5de938ad8c1752
Alixo <fromiran98@gmail.com>
2021-03-04 08:24:40 +0330

    1.0.2

--- alixo1/blog/views.py
+++ alixo1/blog/views.py
@@ -2,5 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def home(request):
-    return render(request,"blog/home.html")
+    context={
+        "articles":[
+            {
+                "title":"HI",
+                "id":12,
+                "content": "Hello World"
+            },{
+                "title":"Hello",
+                "id":13,
+                "content": "Hello Guys"
+            },{
+                "title":"Hello",
+                "id":13,
+                "content": "Hello Guys"
+            }
+        ]
+    }
+    return render(request,"blog/home.html",context)
